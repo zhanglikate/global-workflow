bd3c6ec39c4238673c8c36300c96dad2a8491941
russ.treadon <Russ.Treadon@noaa.gov>
2019-07-17 21:08:44 +0000

    Vlab issue: #66132
    update sorc/checkout.sh to check out ProdGSI branch DA_GFS_v15.2

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -21,7 +21,7 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
     cd gsi.fd
-    git checkout fv3da.v1.0.43
+    git checkout DA_GFS_v15.2
     git submodule update
     cd ${topdir}
 else
