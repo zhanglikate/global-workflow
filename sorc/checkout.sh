4b6e4b3aa657d7dcd1f95defc414fc56ef700135
Jiande.Wang <Jiande.Wang@noaa.gov>
2019-11-08 09:44:00 -0500

    add all 4 plan options in checkout.sh

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,10 +9,22 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
 #    git clone --recursive gerrit:EMC_FV3-MOM6-CICE5 fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
 #   remove recursive based on kate hotfix https://vlab.ncep.noaa.gov/redmine/issues/67072
+
     git clone gerrit:EMC_FV3-MOM6-CICE5 fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-#    git checkout --track origin/bugfix/fv3atmcplflx
-    git checkout --track origin/bm3fv30606bf
+
+# plan A use all top of master for each component
+#     git checkout --track origin/bugfix/fv3atmcplflx
+
+# plan B    use FV3-V0606 (note: Partha's run used branch UFS-FV3-V0606-CPC-ice-pre-BM3, not this one)
+#     git checkout update_fv3_0606_b3
+
+# plan C: use FV3-V0606 pplus bug fixing (gflux=0 and mask ?), results are b4b identical to plan B
+#     git checkout --track origin/bm3fv30606bf
+
+# plan D: use ESRL bug fixing code based on fv3-V0606
+    git checkout --track origin/bm3fv30606bf_PlanD
+
     git submodule update --init --recursive
     cd ${topdir}
 else
