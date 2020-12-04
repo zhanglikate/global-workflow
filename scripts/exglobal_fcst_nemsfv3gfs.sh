52df912d1432d7f00d32be0d1cfaaf6c165e1048
Judy.K.Henderson <Judy.K.Henderson@noaa.gov>
2020-12-04 22:06:31 +0000

    - updated .gitignore file - updated prepchem.sh - default assim_freq set to 24 and OUTPUT_FILE set to nemsio - set value cplchm_rad_opt to T in config.fcst to turn aerosol feedback on - removed fv3gfs_emc.fd from checkout script - set default value for cplchm_rad_opt to F in namelist in exglobal forecast script - modified for statement for sfcanl files in exglobal forecast script - added experiment setup scripts - added experiment directory for realtime runs (FV3GFSwfm/rt_ccpp-chem)

--- scripts/exglobal_fcst_nemsfv3gfs.sh
+++ scripts/exglobal_fcst_nemsfv3gfs.sh
@@ -284,7 +284,8 @@ if [ $warm_start = ".true." -o $RERUN = "YES" ]; then
     done
 
   # Link sfcanl_data restart files from $memdir
-    for file in $(ls $memdir/RESTART/${sPDY}.${scyc}0000.*.nc); do
+    #for file in $(ls $memdir/RESTART/${sPDY}.${scyc}0000.*.nc); do   #lzhang
+    for file in $memdir/RESTART/${sPDY}.${scyc}0000.*.nc; do
       file2=$(echo $(basename $file))
       file2=$(echo $file2 | cut -d. -f3-) # remove the date from file
       fsufanl=$(echo $file2 | cut -d. -f1)
@@ -1124,7 +1125,7 @@ deflate_level=${deflate_level:-1}
   do_shum      = ${do_shum:-".false."}
   do_skeb      = ${do_skeb:-".false."}
   fscav_aero   = "sulf:0.2", "bc1:0.2","bc2:0.2","oc1:0.2","oc2:0.2",
-  cplchm_rad_opt=F
+  cplchm_rad_opt= ${cplchm_rad_opt:="F"}
   aer_bc_opt=1
   aer_ic_opt=1
   aer_ra_feedback=2
--- ush/rocoto/rt-ccpp-chem.sh
+++ ush/rocoto/rt-ccpp-chem.sh
@@ -1,15 +1,13 @@
-USER=Judy.K.Henderson
-PTMP=/scratch1/BMC/gsd-fv3-dev/NCEPDEV/stmp3/$USER               ## default PTMP directory
-STMP=/scratch1/BMC/gsd-fv3-dev/NCEPDEV/stmp4/$USER               ## default STMP directory
-GITDIR=/scratch1/BMC/gsd-fv3-dev/$USER/gsd-ccpp-chem             ## where your git checkout is located
+USER=rtfim
+GITDIR=/scratch1/BMC/gsd-fv3/rtruns/CCPP-Chem                    ## where your git checkout is located
 COMROT=$GITDIR/FV3GFSrun                                         ## default COMROT directory
 EXPDIR=$GITDIR/FV3GFSwfm                                         ## default EXPDIR directory
 
-#    ICSDIR is assumed to be under $COMROT/FV3ICS
+#    ICSDIR is assumed to be under $GITDIR/FV3ICS
 
-PSLOT=GFSv15
-IDATE=2020010700
-EDATE=2020010700
+PSLOT=rt_ccpp-chem
+IDATE=2020111500
+EDATE=2030011000
 RESDET=384               ## 96 192 384 768
 
 ### gfs_cyc 1  00Z only;  gfs_cyc 2  00Z and 12Z
@@ -25,3 +23,5 @@ RESDET=384               ## 96 192 384 768
 # call jobs/rocoto/makefv3ic_link.sh for fv3ic task
 sed -i "s/fv3ic.sh/makefv3ic_link.sh/" $EXPDIR/$PSLOT/$PSLOT.xml
 
+# call jobs/rocoto/arch_gsd.sh for gfsarch task
+sed -i "s/arch.sh/arch_gsd.sh/" $EXPDIR/$PSLOT/$PSLOT.xml
