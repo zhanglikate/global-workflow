56bd6583bd6eac6db05fb7868c67ed8c8222e3cd
Judy.K.Henderson <Judy.K.Henderson@noaa.gov>
2021-04-13 18:12:06 +0000

    - added linking to FV3-Chem regrid directory in calcinc_gfsv16.sh - added calcinc_gfsv16.sh-retro file - check to see if $DATA directory exists in calcinc v16 scripts; if not, create it - added CCPP-C384   -- new experiment setup script, ush/rocoto/rt-ccpp_c384.sh   -- new realtime experiment directory for CCPP-C384, FV3GFSwfm/rt_ccpp_c384   -- new field table, parm/parm_fv3diag/field_table_gfdl_gsd   -- new global forecast script, exglobal_fcst_nemsfv3gfs_nochem.sh   -- add FV3_GFS_v15 suite when compiling fv3_ccpp_chem

--- sorc/build_fv3_ccpp_chem.sh
+++ sorc/build_fv3_ccpp_chem.sh
@@ -25,6 +25,6 @@ cd tests/
 #./compile.sh  "$target" "CCPP=Y 32BIT=Y STATIC=Y DEBUG=Y SUITES=FV3_GFS_v15" 2 NO NO  #minimum 
 #./compile.sh "$target" "CCPP=Y 32BIT=Y STATIC=Y SUITES=FV3_GFS_v15" 2 YES YES  #clean everything and recompile
 #./compile.sh "$target" "CCPP=Y 32BIT=Y STATIC=Y SUITES=FV3_GFS_2017_gfdlmp_gsd_chem" 2 YES YES  #clean everything and recompile
-./compile.sh "$target" "CCPP=Y 32BIT=Y STATIC=Y SUITES=FV3_GFS_v15_gsd_chem" 2 YES YES  #clean everything and recompile
+./compile.sh "$target" "CCPP=Y 32BIT=Y STATIC=Y SUITES=FV3_GFS_v15,FV3_GFS_v15_gsd_chem" 2 YES YES  #clean everything and recompile
 #./compile.sh "$target" "REPRO=Y CCPP=Y STATIC=Y SUITES=FV3_GFS_v15" 2 YES YES  #clean everything and recompile (regression test way)
 mv -f fv3_2.exe ../NEMS/exe/global_fv3gfs_ccpp.x
